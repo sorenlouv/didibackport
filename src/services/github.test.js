8a21c49d4aa4b35fdfce72f0a399702fdbdeea59
Søren Louv-Jansen <sorenlouv@gmail.com>
2018-02-13 15:28:51 +0100

    Fix tests

--- src/services/github.test.js
+++ src/services/github.test.js
@@ -1,19 +1,50 @@
-import { sortRepositories } from './github';
+import { getSortedRepos } from './github';
 import '../../tests/browserMocks';
 
-describe('sortRepositories', () => {
+describe('getSortedRepos', () => {
   it('it should sort repos by counter ', () => {
     global.localStorage.setItem(
       'repositories_with_counter',
       JSON.stringify({
-        '1': 100,
-        '2': 10,
-        '3': 50
+        elasticsearch: 100,
+        kibana: 10,
+        'x-pack-kibana': 50
       })
     );
 
     expect(
-      sortRepositories([{ id: 4 }, { id: 1 }, { id: 2 }, { id: 3 }])
-    ).toEqual([{ id: 1 }, { id: 3 }, { id: 2 }, { id: 4 }]);
+      getSortedRepos([
+        { name: 'beats' },
+        { name: 'elasticsearch' },
+        { name: 'x-pack-kibana' },
+        { name: 'kibana' }
+      ])
+    ).toEqual([
+      { name: 'elasticsearch' },
+      { name: 'x-pack-kibana' },
+      { name: 'kibana' },
+      { name: 'beats' }
+    ]);
+  });
+
+  it('it should leave unordered repos unchanged', () => {
+    global.localStorage.setItem(
+      'repositories_with_counter',
+      JSON.stringify({ elasticsearch: 1 })
+    );
+
+    expect(
+      getSortedRepos([
+        { name: 'beats' },
+        { name: 'elasticsearch' },
+        { name: 'x-pack-kibana' },
+        { name: 'kibana' }
+      ])
+    ).toEqual([
+      { name: 'elasticsearch' },
+      { name: 'beats' },
+      { name: 'x-pack-kibana' },
+      { name: 'kibana' }
+    ]);
   });
 });
